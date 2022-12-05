4f2bf064c9dc77bac000d853db9a6d2a6b4e0e5e
Nick.Hopgood <nickhopgood@gmail.com>
2022-12-05 14:37:17 +0000

    Day 2 part 2.

---
+++ day2/day2_pt2.py
@@ -0,0 +1,35 @@
+file = open('strategy_guide.txt', 'r')
+lines = file.readlines()
+
+# Method 1 iterate through all games and assign hand based on desired result and opponents hand.
+hand = {
+    "rock": 1,
+    "paper": 2,
+    "scissors": 3
+}
+
+result = []
+for line in lines:
+    line = line.strip()
+    if "X" in line:
+        if "A" in line:
+            result.append(hand["scissors"])
+        elif "B" in line:
+            result.append(hand["rock"])
+        elif "C" in line:
+            result.append(hand["paper"])
+    elif "Y" in line:
+        if "A" in line:
+            result.append(hand["rock"] + 3)
+        elif "B" in line:
+            result.append(hand["paper"] + 3)
+        elif "C" in line:
+            result.append(hand["scissors"] + 3)
+    elif "Z" in line:
+        if "A" in line:
+            result.append(hand["paper"] + 6)
+        elif "B" in line:
+            result.append(hand["scissors"] + 6)
+        elif "C" in line:
+            result.append(hand["rock"] + 6)
+print(sum(result))
