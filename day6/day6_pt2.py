2f4c63cb26f6fd45c9139ca0fcb748e25520ebcb
Nick.Hopgood <nickhopgood@gmail.com>
2022-12-07 15:19:43 +0000

    Day 6 part 2 refactor.

--- day6/day6_pt2.py
+++ day6/day6_pt2.py
@@ -1,3 +1,11 @@
+# Calculate unique string character length check
+def unique_string(number):
+    required_char = 0
+    for x in range(1, number + 1):
+        required_char += number - 1
+        number -= 1
+    return required_char
+
 
 # Day 6 part 1 function
 def day6_pt2():
@@ -5,7 +13,7 @@ def day6_pt2():
     data = file.read()
     file.close()
     count = 0
-    print(len(data))
+    required_length = unique_string(14)
     for char in range(0, len(data) - 15):
         start = 0
         count += 1
@@ -14,8 +22,7 @@ def day6_pt2():
             for x in range(start, 13):
                 if data[char + start] != data[char + (x + 1)]:
                     char_count += 1
-                    if char_count == 91:
+                    if char_count == required_length:
                         count += 13
-                        print(count)
                         return count
             start += 1
